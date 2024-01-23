cf766fdc0208cc3dfe108070adfc96d0fbd4e487
Piwong1 <142533789+Piwong1@users.noreply.github.com>
2024-01-23 11:19:55 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -6,7 +6,7 @@ public class PalindromeChecker {
 public void tester()
 {
   //String lines[] = loadStrings("palindromes.txt");
-  String[] lines = new String{"test","rotator","rewriter","nurses run","Madam, I'm Adam!","A Man! A Plan! A Canal! Panama!"};
+  String[] lines = new String[6];
     try{
         File myFile = new File("palindromes.txt");
         Scanner myReader = new Scanner(myFile);
